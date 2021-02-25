3388704d8e4636e8c5c454bab0c8d73f3b2832eb
fernando.mendez <fernando.mendez@upr.edu>
2021-02-25 19:07:11 -0400

    Made changes to main.java

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,18 @@ import aima.Core.environment.*;
 public class Main {
 	
 	public static void main(String[] args) {
+		EnvironmentListener<Object, Object> vacuumViewer = new SimpleEnvironmentView();
+		Environment<VacuumPercept, Action> vacuumEnv = new VacuumEnvironment().addEnvironmentListener(vacuumViewer);
+		
+		Agent simpleAgent 	= new SimpleReflexVacuumAgent();
+		Agent tableAgent 	= new TableDrivenVacuumAgent();
+		Agent modelAgent 	= new ModelBaderVacuumAgent();
+		
+		VacuumEnvironment cleanClean = new VacuumEnvironment(VacuumEnvironment.LocationState.Clean, VacuumEnvironment.LocationState.Clean);
+		VacuumEnvironment cleanDirty = new VacuumEnvironment(VacuumEnvironment.LocationState.Clean, VacuumEnvironment.LocationState.Dirty);
+		VacuumEnvironment dirtyClean = new VacuumEnvironment(VacuumEnvironment.LocationState.Dirty, VacuumEnvironment.LocationState.Clean);
+		VacuumEnvironment dirtyDirty = new VacuumEnvironment(VacuumEnvironment.LocationState.Dirty, VacuumEnvironment.LocationState.Dirty);
+		
 		
 	}
 
